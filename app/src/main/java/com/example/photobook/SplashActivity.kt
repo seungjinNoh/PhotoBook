08175b6ccc280db85fd36a19460a7b05e354f3dd
seungjin <seungjin3047@gmail.com>
2023-11-05 20:38:50 +0900

    feat: SplashActivity 위치 권한 추가

--- app/src/main/java/com/example/photobook/SplashActivity.kt
+++ app/src/main/java/com/example/photobook/SplashActivity.kt
@@ -1,40 +1,59 @@
 package com.example.photobook
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.photobook.ui.theme.PhotoBookTheme
-import com.gun0912.tedpermission.PermissionListener
-import com.gun0912.tedpermission.normal.TedPermission
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.photobook.utils.LogUtil
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (checkPermissions(permissions)) {
+            LogUtil.d("권한이 이미 부여됨")
+        } else {
+            requestPermissions(permissions)
+        }
+
     }
 
-    private fun checkPermission(logic: () -> Unit) {
-        TedPermission.create()
-            .setPermissionListener(object : PermissionListener {
-                override fun onPermissionGranted() {
-                    logic()
-                }
+    private fun checkPermissions(permissions: Array<String>): Boolean {
+        val grantedPermissions = permissions.filter {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+        return grantedPermissions.size == permissions.size
+    }
 
-                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+    private fun requestPermissions(permissions: Array<String>) {
+        ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE)
+    }
 
-                }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // 모든 권한이 허용됨
+                LogUtil.d("모든 권한이 허용됨")
+            } else {
+                // 권한이 거부됨
+                LogUtil.d("권한이 거부됨")
+            }
+        }
+    }
 
-            })
-            .setPermissions(
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 1
     }
 }
